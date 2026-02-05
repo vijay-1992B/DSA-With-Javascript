4632eb1b84f9c2fe744c0b71dbb8a46983d20c1c
vijay-1992B <vj462856@gmail.com>
2026-02-05 13:03:30 +0530

    Add heap introduction notes and basic concepts

---
+++ heap-priority-queue/introduction.js
@@ -0,0 +1,43 @@
+/*
+=================================
+        HEAP INTRODUCTION
+=================================
+
+A Heap is a special type of Binary Tree.
+
+👉 It follows 2 main rules:
+
+1️⃣ Heap is a COMPLETE BINARY TREE
+   - All levels are filled
+   - Last level fills from LEFT to RIGHT
+
+2️⃣ Heap Property (Two Types)
+
+   🔹 Max Heap:
+      Parent node is GREATER than its children
+      Example:
+            50
+           /  \
+         30    40
+
+   🔹 Min Heap:
+      Parent node is SMALLER than its children
+      Example:
+            10
+           /  \
+         20    30
+
+---------------------------------
+
+   Advantages of Heap
+- Time complexity to finding the maximum value in Max Heap is O(1)
+- Time complexity to finding the minimum value in Min Heap is O(1)
+- Time complexity to Inserting element in Heap is O(log n)
+- Time complexity to Deleting element in Heap is O(log n)
+
+---------------------------------
+
+This file is only for understanding.
+We are NOT implementing heap yet.
+That will come next step by step.
+*/
