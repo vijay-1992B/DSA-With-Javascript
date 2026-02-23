e84a12641ed48a7cd16dc166cb830ee814bef5ae
vijay-1992B <vj462856@gmail.com>
2026-02-23 14:30:01 +0530

    Implement Max Priority Queue using Heap

--- heap-priority-queue/priorityQueue.js
+++ heap-priority-queue/priorityQueue.js
@@ -43,3 +43,86 @@ pq.enqueue("Accident", 5);
 pq.enqueue("Headache", 1);
 
 console.log(pq.queue);
+
+// Implementation 2: Using Heap
+
+class MaxPriorityQueue {
+  constructor() {
+    this.heap = [];
+  }
+
+  enqueue(value, priority) {
+    this.heap.push({ value, priority });
+    this.heapifyUp();
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (index > 0) {
+      let parent = Math.floor((index - 1) / 2);
+      if (this.heap[index].priority < this.heap[parent].priority) break;
+      this.swap(index, parent);
+      index = parent;
+    }
+  }
+
+  dequeue() {
+    if (this.heap.length === 0) return null;
+    let max = this.heap[0];
+    let end = this.heap.pop();
+    if (this.heap.length > 0) {
+      this.heap[0] = end;
+      this.heapifyDown();
+    }
+    return max;
+  }
+
+  heapifyDown() {
+    let index = 0;
+    let length = this.heap.length;
+    while (true) {
+      let leftChild = 2 * index + 1;
+      let rightChild = 2 * index + 2;
+      let largest = index;
+
+      if (
+        leftChild < length &&
+        this.heap[leftChild].priority > this.heap[largest].priority
+      ) {
+        largest = leftChild;
+      }
+      if (
+        rightChild < length &&
+        this.heap[rightChild].priority > this.heap[largest].priority
+      ) {
+        largest = rightChild;
+      }
+      if (largest === index) break;
+      this.swap(index, largest);
+      index = largest;
+    }
+  }
+
+  front() {
+    return this.heap.length > 0 ? this.heap[0] : null;
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+
+  // swap helper
+  swap(i, j) {
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+  }
+}
+
+let pQ = new MaxPriorityQueue();
+pQ.enqueue("Fever", 2);
+pQ.enqueue("Headache", 1);
+pQ.enqueue("Accident", 5);
+pQ.dequeue();
